7db6f763fe96636164fa04f119ec5e6ab418e852
NaimMhedhbi <naiim.mhedhbi@gmail.com>
2023-01-23 12:30:30 +0100

    added files

--- connect_to_database.py
+++ connect_to_database.py
@@ -1,7 +1,10 @@
 import pyodbc 
-conn = pyodbc.connect('Driver={SQL Server};'
+conn = pyodbc.connect('Driver=SQL Server'
                       'Server=DESKTOP-QIQG2OF\SQLEXPRESS'
                       'Database=tests'
                       'Trusted_Connection=yes')
 
 cursor = conn.cursor()
+
+
+#server name : DESKTOP-QIQG2OF\SQLEXPRESS
